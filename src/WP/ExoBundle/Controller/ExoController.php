f96571ee33a739a05bfff9c62aca8524f926d6b5
sandman1602 <sandmandps@gmail.com>
2017-01-26 21:09:55 +0100

    change

--- src/WP/ExoBundle/Controller/ExoController.php
+++ src/WP/ExoBundle/Controller/ExoController.php
@@ -425,6 +425,42 @@ class ExoController extends Controller
         return new Response();
 
     }
+
+
+
+
+//    public function mailerAction()
+//    {
+//
+//
+//         $message = \Swift_Message::newInstance()
+//        ->setSubject('Hello Email')
+//        ->setFrom('send@example.com')
+//        ->setTo('recipient@example.com')
+//        ->setBody(
+//            $this->renderView(
+//                // app/Resources/views/Emails/registration.html.twig
+//                'Emails/registration.html.twig',
+//                array('name' => $name)
+//            ),
+//            'text/html'
+//        )
+//        /*
+//         * If you also want to include a plaintext version of the message
+//        ->addPart(
+//            $this->renderView(
+//                'Emails/registration.txt.twig',
+//                array('name' => $name)
+//            ),
+//            'text/plain'
+//        )
+//        */
+//    ;
+//    $this->get('mailer')->send($message);
+//
+//    return $this->render(...);
+//
+//    }
 }
 
 
